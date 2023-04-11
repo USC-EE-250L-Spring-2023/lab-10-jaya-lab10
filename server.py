f9ab2180da76e628475fcf2ae3532f68fbcd33d3
jmtravis <122946408+jmtravis@users.noreply.github.com>
2023-04-11 16:53:07 -0700

    Update server.py

--- server.py
+++ server.py
@@ -11,3 +11,13 @@ def index():
 
 # TODO: Create a flask app with two routes, one for each function.
 # The route should get the data from the request, call the function, and return the result.
+
+@app.route('/process1', methods = 'POST')
+def process1():
+    data = request.json
+    return jsonify(data)
+
+@app.route('/process2', methods = 'POST')
+def process2():
+    data = request.json
+    return jsonify(data)
