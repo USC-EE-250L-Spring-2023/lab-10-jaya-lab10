5bf50748e63d313948c3f506b95d598f98823b33
jmtravis <122946408+jmtravis@users.noreply.github.com>
2023-04-14 15:16:10 -0700

    Update main.py

--- main.py
+++ main.py
@@ -103,10 +103,11 @@ def run(offload: Optional[str] = None) -> float:
             # TODO: Send a POST request to the server with the input data
             data1 = response.json()
             send1 = requests.post(f"{offload_url}/receive1", json = data1
+            data2 = None
         thread1 = threading.Thread(target=offload_process1, args=(data,))
         thread1.start() 
         thread1.join()
-        data2 = None
+        
         def offload_process2(data):
             nonlocal data2
             # TODO: Send a POST request to the server with the input data
