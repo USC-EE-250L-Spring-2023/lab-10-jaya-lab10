aad8eb423b5b6f7c02c67c663cbf7a775870f9c4
jmtravis <122946408+jmtravis@users.noreply.github.com>
2023-04-14 15:05:37 -0700

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def generate_data() -> List[int]:
 
 def process1(data: List[int]) -> List[int]:
     """TODO: Document this function. What does it do? What are the inputs and outputs?"""
-      """Run the program, offloading the specified function(s) to the server.
+    """Run the program, offloading the specified function(s) to the server.
     
     Args:
         offload: Which function(s) to offload to the server. Can be None, 'process1', 'process2', or 'both'.
