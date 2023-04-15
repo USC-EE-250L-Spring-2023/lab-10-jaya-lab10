73317d5eaf4300477387aff86327420ea7324546
jmtravis <122946408+jmtravis@users.noreply.github.com>
2023-04-14 17:49:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -134,7 +134,7 @@ def main():
                     run(mode)
                     end = time.time()
                     times.append(start - end)
-                    print(f"Offloading {mode} - sample {i+1}: {times[-1]:.sf}" )
+                    print(f"Offloading {mode} - sample {i+1}: {times[-1]:.2f}" )
                     rows.append([mode],np.mean(times), np.std(times))        
                  
                  
